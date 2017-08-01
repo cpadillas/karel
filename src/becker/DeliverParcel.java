7e5a23c34200a69bbf48c232fca0294189e2305b
leotalorac <lwotalorac@unal.edu.co>
2017-08-01 16:02:29 -0500

    parcial

--- src/becker/DeliverParcel.java
+++ src/becker/DeliverParcel.java
@@ -8,20 +8,29 @@ public class DeliverParcel
    {  
      // Set up the initial situation
       City prague = new City();
-      Thing parcel = new Thing(prague, 1, 2);
-      Robot karel = new Robot(prague, 1, 0, Direction.EAST,5);
+      Thing t1 = new Thing(prague, 1, 2);
+      Thing t2 = new Thing(prague, 2, 2);
+      Robot karel = new Robot(prague, 1, 0, Direction.EAST,20);
+      Wall p1 = new Wall(prague, 1, 2, Direction.WEST);
     
       // Direct the robot to the final situation
       karel.move();
+      
+      karel.putThing();
       karel.move();
-      karel.pickThing();
-      karel.move();
-      karel.turnLeft();	// start turning right as three turn lefts
-      karel.turnLeft();
-      karel.turnLeft();	// finished turning right
+      	// finished turning right
       karel.move();
      // karel.putThing();
       karel.move();
       System.out.println(karel.countThingsInBackpack());
+      
+      
+      
+      /*karel.canPickThing();
+      karel.pickThing();
+      karel.putThing();
+      karel.move();
+      karel.turnLeft();
+      karel.frontIsClear();*/
    }
 }
